0c4a15aa59cbe9cacd7be46d2049bc5bc6bf2596
Aileen Villalpando <avillalpando@college.harvard.edu>
2017-12-07 02:04:25 +0000

    eighth commit

---
+++ static/scripts.js
@@ -0,0 +1,26 @@
+// $( document ).ready(function() { //wait until body loads
+
+// 		var meal_chosen=false; //keeps track of whether the testimonial box is filled out
+
+// 		//Inputs that determine what fields to show
+// 		var meal = $('#live_form input:radio[name=meal]');
+
+// 		meal.change(function(){ //when the meal changes
+// 			var value=this.value;
+// 			all.addClass('hidden'); //hide everything and reveal as needed
+
+// 			if (meal_chosen==true){
+// 				name.removeClass('hidden'); //show recipes
+// 			}
+// 		});
+// });
+
+function showRecipes(ingredients)
+{
+		var input
+
+}
+
+$(".list_item").onclick(function() {
+	console.log("blah");
+});
